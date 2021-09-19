b86cd2781c6e185890e359fdaa41bd0e6061bfcb
ann <anya.piryakina@gmail.com>
2021-09-19 08:16:39 +0300

    bib

---
+++ src/additional/First.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function First() {
+    let url = "";
+    return(
+        <div>
+            <a href={url}>Hello World blin</a>
+        </div>
+    )
+
+}
